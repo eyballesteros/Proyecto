92d202b0b3d6921623d9ea9579ecf65765c50b48
Yessid <eyballesterosp@gmail.com>
2024-10-16 21:31:33 -0500

    16-10-2024

--- pantallaLed.php
+++
@@ -1,6 +0,0 @@
-<?php include("./template/cabecera.php");?>
-<div class="sitioEnMantenimiento">
-    <img src="./assets/EnMantenimiento.png" alt="Sitio en Mantenimiento">
-</div>
-
-<?php include("./template/pie.php");?>
